70ca12ce372bc5e9ac2050b884c45515c8cfdc49
vincentkovalski <kovall89@gmail.com>
2022-05-17 19:07:42 +0500

    Changing the background color to red

--- homework-git/configViewController.swift
+++ homework-git/configViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  configViewController.swift
 //  homework-git
 //
 //  Created by Виктор Ковалевский on 17.05.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConfigViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+
     }
-
-    
 }
-
